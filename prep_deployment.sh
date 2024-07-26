65438bfce32a1f4bf0cfe745719a155162315b4f
Miraj Patel <mirag@MSI-DESKTOP>
2024-07-25 23:11:18 -0400

    Fix canonical URLs and build command

--- prep_deployment.sh
+++ prep_deployment.sh
@@ -6,7 +6,7 @@ rm -r ./static/css
 
 
 echo "Building app..."
-NODE_OPTIONS=--openssl-legacy-provider npm run start
+NODE_OPTIONS=--openssl-legacy-provider npm run build
 
 echo "Copying built files from dist into current directory"
 cp -r ./dist/** .
@@ -15,6 +15,11 @@ routes=("/about" "/experiences")
 for route in ${routes[@]}; do
   echo $route
   mkdir -p "$(dirname "$0")$route/" && cp "$(dirname "$0")/index.html" "$_"
+  sed -i "s;rel=\"canonical\" href=\"https://miraj.dev/\";rel=\"canonical\" href=\"https://miraj.dev$route/\";g" "$(dirname "$0")$route/index.html"
+  sed -i "s;property=\"og:url\" content=\"https://miraj.dev/\";property=\"og:url\" content=\"https://miraj.dev$route/\";g" "$(dirname "$0")$route/index.html"
+
+  sed -i "s;rel=canonical href=https://miraj.dev/ ;rel=canonical href=https://miraj.dev$route/ ;g" "$(dirname "$0")$route/index.html"
+  sed -i "s;property=og:url content=https://miraj.dev/ ;property=og:url content=https://miraj.dev$route/ ;g" "$(dirname "$0")$route/index.html"
 done
 
 echo "Done!"
