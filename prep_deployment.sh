0c0f280c1352de808599e7cb489bd8b171cbf4a0
mirajp <mirage.patel@gmail.com>
2024-07-25 22:51:35 -0400

    Add static HTML for paths by copying index.html for each route

--- prep_deployment.sh
+++ prep_deployment.sh
@@ -6,9 +6,15 @@ rm -r ./static/css
 
 
 echo "Building app..."
-npm run build
+NODE_OPTIONS=--openssl-legacy-provider npm run start
 
 echo "Copying built files from dist into current directory"
 cp -r ./dist/** .
+
+routes=("/about" "/experiences")
+for route in ${routes[@]}; do
+  echo $route
+  mkdir -p "$(dirname "$0")$route/" && cp "$(dirname "$0")/index.html" "$_"
+done
 
 echo "Done!"
